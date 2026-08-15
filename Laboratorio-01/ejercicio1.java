2bd74f1a5768ed75ab01b2edfca9204e8cf72de0
jorzu29 <jorzu2907@gmail.com>
2026-08-15 12:46:25 -0500

    Create ejercicio1.java

---
+++ Laboratorio-01/ejercicio1.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+	int [] arreglo = {1, 2, 3, 4, 100};
+	sumarArreglo(arreglo);
+}
+	
+public static void sumarArreglo (int[] arreglo) {
+	int suma = 0;
+	for (int i=0; i < arreglo.length; i++) {
+		suma += arreglo[i];
+	}
+	System.out.println("La suma total del arreglo es: " + suma);
+}
+
