bae1b13714749a922029ae0bf98c4c7edf0eb701
jorzu29 <jorzu2907@gmail.com>
2026-08-15 12:50:25 -0500

    Implement input and find minimum number functionality

---
+++ Laboratorio-01/ejercicio4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+		
+	double[] numeros = new double[3];
+		
+	for (int i=0; i < numeros.length; i++) {
+		System.out.println("Introduzca numero N°" + (i+1) +": ");
+		numeros[i] = scanner.nextDouble();
+	}
+	numeroMenor(numeros);
+}
+	
+public static void numeroMenor(double[] numeros) {
+	double menor = Double.MAX_VALUE;
+	for (int i=0; i < numeros.length; i++ ) {
+		if (numeros[i] < menor) {
+			menor = numeros[i];
+		}
+	}
+	System.out.println("El menor numero es: " + menor);
+}
