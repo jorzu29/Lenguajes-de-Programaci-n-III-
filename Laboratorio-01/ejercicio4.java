ac87191b5be138667d89dad7a1fbc0b7240005c8
jorzu29 <jorzu2907@gmail.com>
2026-08-16 13:41:13 -0500

    Refactor ejercicio4.java for class structure

--- Laboratorio-01/ejercicio4.java
+++ Laboratorio-01/ejercicio4.java
@@ -1,23 +1,27 @@
+package prueba;
 import java.util.Scanner;
 
-public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-		
-	double[] numeros = new double[3];
-		
-	for (int i=0; i < numeros.length; i++) {
-		System.out.println("Introduzca numero N°" + (i+1) +": ");
-		numeros[i] = scanner.nextDouble();
-	}
-	numeroMenor(numeros);
-}
+public class menorDecimal {
 	
-public static void numeroMenor(double[] numeros) {
-	double menor = Double.MAX_VALUE;
-	for (int i=0; i < numeros.length; i++ ) {
-		if (numeros[i] < menor) {
-			menor = numeros[i];
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		double[] numeros = new double[3];
+		
+		for (int i=0; i < numeros.length; i++) {
+			System.out.println("Introduzca numero N°" + (i+1) +": ");
+			numeros[i] = scanner.nextDouble();
 		}
+		numeroMenor(numeros);
 	}
-	System.out.println("El menor numero es: " + menor);
+	
+	public static void numeroMenor(double[] numeros) {
+		double menor = Double.MAX_VALUE;
+		for (int i=0; i < numeros.length; i++ ) {
+			if (numeros[i] < menor) {
+				menor = numeros[i];
+			}
+		}
+		System.out.println("El menor numero es: " + menor);
+	}
 }
