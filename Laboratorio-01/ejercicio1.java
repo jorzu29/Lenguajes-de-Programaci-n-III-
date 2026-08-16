da0d7d8a0cc8d6e90921cfd2c53b64ac1344c1b7
jorzu29 <jorzu2907@gmail.com>
2026-08-16 13:40:39 -0500

    Refactor ejercicio1.java to include package and class

--- Laboratorio-01/ejercicio1.java
+++ Laboratorio-01/ejercicio1.java
@@ -1,13 +1,17 @@
-public static void main(String[] args) {
-	int [] arreglo = {1, 2, 3, 4, 100};
-	sumarArreglo(arreglo);
-}
-	
-public static void sumarArreglo (int[] arreglo) {
-	int suma = 0;
-	for (int i=0; i < arreglo.length; i++) {
-		suma += arreglo[i];
-	}
-	System.out.println("La suma total del arreglo es: " + suma);
-}
+package prueba;
+public class SumaArreglos {
 
+	public static void main(String[] args) {
+		int [] arreglo = {1, 2, 3, 4, 100};
+		sumarArreglo(arreglo);
+	}
+	
+	public static void sumarArreglo (int[] arreglo) {
+		int suma = 0;
+		for (int i=0; i < arreglo.length; i++) {
+			suma += arreglo[i];
+		}
+		System.out.println("La suma total del arreglo es: " + suma);
+	}
+
+}
