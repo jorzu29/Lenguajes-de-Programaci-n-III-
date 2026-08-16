0643ade6671684da0619b8355ad3a30894b202fc
jorzu29 <jorzu2907@gmail.com>
2026-08-16 13:59:49 -0500

    Add function to sum elements of an array

--- Laboratorio-01/ejercicio1.py
+++ Laboratorio-01/ejercicio1.py
@@ -1,1 +1,9 @@
+def sumarArreglo (arreglo):
+    suma = 0;
+    
+    for i in range(0, len(arreglo)):
+        suma += arreglo[i]
+    print(f"La suma total del arreglo es: {suma}")
 
+arreglo = [1,2,3,4,100];
+sumarArreglo(arreglo)
