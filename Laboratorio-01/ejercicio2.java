76334cdc78d69e1fe946a5b68c2f7b4cab7bd440
danielgonxales <kevinrex823@gmail.com>
2026-08-15 23:40:18 -0500

    Update ejercicio2.java

--- Laboratorio-01/ejercicio2.java
+++ Laboratorio-01/ejercicio2.java
@@ -1,17 +1,13 @@
-package ejercicio;
-import java.util.Random;
+public static void main(String[]args) {
+      Random random=new Random();
+  int[] numeros = new int[20000];
 
-public class ejercicio {
-      public static void main(String[]args) {
-            Random random=new Random();
-        int[] numeros = new int[20000];
-
-            for(int i=0;i<20000;i++) {
-                  int numero=random.nextInt(6)+1;
-                  numeros[i] = numero;
-            }
-          for (int j = 0; j < numeros.length; j++) {
-            System.out.println(numeros[j]);
-          }
-      }
+      for(int i=0;i<20000;i++) {
+            int numero=random.nextInt(6)+1;
+            numeros[i] = numero;
+      }
+    for (int j = 0; j < numeros.length; j++) {
+      System.out.println(numeros[j]);
+    }
 }
+
