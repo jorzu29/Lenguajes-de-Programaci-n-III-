b7eda569e55983f53881d9f50c448d60dde1c2b5
jorzu29 <jorzu2907@gmail.com>
2026-08-22 22:08:52 -0500

    Add Estudiante class extending Persona

---
+++ Laboratorio 02/Estudiante.java
@@ -0,0 +1,22 @@
+package SistemaGestion;
+
+public class Estudiante extends Persona {
+	private String matricula;
+	
+	private static int contadorEstudiantes = 0;
+	
+	public Estudiante(String id, String nom, String em, String mat) {
+		super(id, nom, em);
+		this.matricula = mat;
+		contadorEstudiantes++;
+	}
+	
+	public static int getContadorEstudiante() {
+		return contadorEstudiantes;
+	}
+	
+	@Override
+	public String mostrarAtributos() {
+		return "Nombre de Estudiante: " + getNombre() + " | Matricula: " + matricula;
+	}
+}
