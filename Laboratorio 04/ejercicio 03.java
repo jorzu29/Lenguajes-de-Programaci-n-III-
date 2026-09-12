13164198af6d6a3c5888fcf19d5e0541f1bf9369
danielgonxales <kevinrex823@gmail.com>
2026-09-11 21:44:40 -0500

    Update ejercicio 03.java

--- Laboratorio 04/ejercicio 03.java
+++ Laboratorio 04/ejercicio 03.java
@@ -1,1 +1,26 @@
+class Numero {
+    private double valor;
 
+    Numero(double valor) {
+        this.valor = valor;
+    }
+    public void setValor(double valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("El valor no puede ser negativo");
+        }
+        this.valor = valor;
+    }
+    public double getValor() {
+        return valor;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Numero num1 = new Numero(0);
+        try {
+            num1.setValor(-2.3);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
